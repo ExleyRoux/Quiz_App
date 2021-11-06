c6f4f00c5f3894e4ccd50555124ffe07483316d3
Exley <exley.roux@gmail.com>
2021-11-06 19:00:49 +0200

    Use of Open Trivia DB API Instead of Local JSON for Question Loading

--- game.js
+++ game.js
@@ -13,14 +13,34 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
 .then(res => {
+    console.log(res);
     return res.json();
 })
 .then(loadedQuestions => {
-    questions = loadedQuestions;
-    startGame();
+    console.log(loadedQuestions);
+    questions = loadedQuestions.results.map(loadedQuestion => {
 
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+        answerChoices.splice(
+            formattedQuestion.answer - 1, 
+            0, 
+            loadedQuestion.correct_answer
+        );
+        
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index+1)] = choice;
+        });
+
+        return formattedQuestion;
+    });
+    startGame();
 })
 .catch(err => {
     console.log(err);
