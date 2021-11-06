349347bcc800ffb2ec025725966a5720f26e38c6
Exley <exley.roux@gmail.com>
2021-11-06 19:19:17 +0200

    Loader and Animation

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 
 let currentQuestion = {};
@@ -56,6 +58,8 @@ startGame = () => {
     availableQuestions = [...questions]; //spreadarray ... for questions array
     console.log(availableQuestions);
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
