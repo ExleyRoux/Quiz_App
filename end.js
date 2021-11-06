ad4e91d1e2ee8d5583425b1ba22b68f40c902b0a
Exley <exley.roux@gmail.com>
2021-11-06 17:52:52 +0200

    Save Scores Into Local Storage

--- end.js
+++ end.js
@@ -2,7 +2,13 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
+
 
 username.addEventListener('keyup', () => {
     console.log(username.value);
@@ -14,4 +20,19 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     console.log("Clicked the save button!");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => {
+        return b.score - a.score;
+    });
+    highScores.splice(5);
+    console.log(highScores);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
+
